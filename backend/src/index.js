c5340bb4ae7063c363a01e574f921d261f47c3f9
Rodrigo R. da Silva <rodrigurdasilva@gmail.com>
2020-01-15 15:58:59 -0300

    Web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-up5rq.mongodb.net/devradar?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster-yu6pp.mongodb.net/devradar?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
 });
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
