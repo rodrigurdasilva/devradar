18939368e6724fb83bd7fdf8aa5195894db888b8
Rodrigo R. da Silva <rodrigurdasilva@gmail.com>
2020-01-17 09:33:56 -0300

    Funcionalidades avançadas

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnecitions, sendMessage } = require('../websocket');
 
 module.exports = {
   async index(request, response) {
@@ -35,6 +36,12 @@ module.exports = {
         location
       });
 
+      const sendSocketMessageTo = findConnecitions(
+        { latitude, longitude },
+        techsArray
+      )
+
+      sendMessage(sendSocketMessageTo, 'new-dev', dev);
     }
 
     return response.json(dev);
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster-yu6pp.mongodb.net/devradar?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -15,4 +21,4 @@ app.use(cors())
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
